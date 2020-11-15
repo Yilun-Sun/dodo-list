9e829574e0219de9f2ddce5f62fde272ad015b8d
YilunSun <osirisnero@gmail.com>
2020-11-15 11:22:05 -0500

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+//   Redirect,
+// } from "react-router-dom";
+import { HashRouter, Route, Link, Redirect } from "react-router-dom";
 import login from "./pages/login";
 import signup from "./pages/signup";
 import home from "./pages/home";
@@ -21,15 +27,39 @@ const theme = createMuiTheme({
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <Router>
+      {/* <Router>
         <div>
           <Switch>
+          <Route exact path="/">
+              <Redirect to="/dodo-list" />
+            </Route>
             <Route exact path="/login" component={login} />
             <Route exact path="/signup" component={signup} />
-            <Route exact path="/" component={home} />
+            <Route exact path="/dodo-list" component={home} />
+            
           </Switch>
         </div>
-      </Router>
+      </Router> */}
+      <HashRouter basename="/">
+        <div>
+          {/* <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+
+          <hr /> */}
+          <Route exact path="/" component={home} />
+          <Route path="/login" component={login} />
+          <Route path="/signup" component={signup} />
+        </div>
+      </HashRouter>
     </MuiThemeProvider>
   );
 }
